cb2523427312bc849dfcb51fc63f1fe5abbd356b
Saleh Haidar <snmh49@gmail.com>
2022-04-28 05:23:58 +0300

    trying putting changes on the new branch

--- JavaScript.js
+++ JavaScript.js
@@ -2,15 +2,29 @@ function computerPlay() {
     return "Paper".toLowerCase();
 }
 
-function round(playerSelection, computerSelection) {
-    if (playerSelection === "paper") {
-        console.log("Tie, all papers");
-    } else if (playerSelection === "rock") {
-        console.log("You Lose! Paper beats Rock");
+function round(e) {
+    if (e.target.className === "paper") {
+        //console.log("Tie, all papers");
+        res.textContent += "tie" + "\tYou: " + playerScore + "\nComputer: " + computerScore;
+    } else if (e.target.className === "rock") {
+        //console.log("You Lose! Paper beats Rock");
         computerScore++;
+        res.textContent += "\nYou Lose! Paper beats Rock" + "\nYou: " + playerScore + "\nComputer: " + computerScore;
     } else {
-        console.log("You Win! Scissor cuts Paper");
+        //console.log("You Win! Scissor cuts Paper");
         playerScore++;
+        res.textContent += "\nYou Win! Scissor cuts Paper" + "\nYou: " + playerScore + "\nComputer: " + computerScore;
+
+    }
+
+    if (computerScore == 5) {
+        res.textContent = "You have lost!";
+        computerScore = 0;
+        playerScore = 0;
+    } else if (playerScore == 5) {
+        res.textContent = "You have won!"
+        computerScore = 0;
+        playerScore = 0;
     }
 
 }
@@ -20,30 +34,42 @@ let playerSelection = "";
 let playerScore = 0;
 let computerScore = 0;
 
-for (let i = 0; i < 5; i++) {
-    while (true) {
+const rock = document.querySelector('.rock');
+rock.addEventListener('click', round);
 
-        playerSelection = prompt("Rock, Paper or Scissor? (write f to quit)").toLowerCase();
-        if (playerSelection == "f") {
-            i = 5;
-            break;
-        }
+const paper = document.querySelector('.paper');
+paper.addEventListener('click', round);
 
-        if (playerSelection != "rock" && playerSelection != "paper"
-            && playerSelection != "scissor") {
-            alert("please write one of the options correctly!");
-        } else {
-            break;
-        }
-    }
+const scissor = document.querySelector('.scissor');
+scissor.addEventListener('click', round);
 
-    console.log(round(playerSelection, computerSelection));
-}
+const container = document.querySelector('.container');
+const res = document.createElement('div');
+//res.textContent = "You: " + playerScore + "\nComputer: " + computerScore;
+container.appendChild(res);
 
-if (playerScore > computerScore) {
-    console.log("Score is\nYou: " + playerScore + "\nComputer: " + computerScore + "\nCongratulations!")
-} else {
-    console.log("Score is\nComputer: " + computerScore + "\nYou: " + playerScore + "\nTry Again :(")
+//for (let i = 0; i < 5; i++) {
+// while (true) {
 
+//     playerSelection = prompt("Rock, Paper or Scissor? (write f to quit)").toLowerCase();
+//     if (playerSelection == "f") {
+//         i = 5;
+//         break;
+//     }
 
-}
+//     if (playerSelection != "rock" && playerSelection != "paper"
+//         && playerSelection != "scissor") {
+//         alert("please write one of the options correctly!");
+//     } else {
+//         break;
+//     }
+// }
+
+//console.log(round(playerSelection, computerSelection));
+//}
+
+// if (playerScore > computerScore) {
+//     console.log("Score is\nYou: " + playerScore + "\nComputer: " + computerScore + "\nCongratulations!")
+// } else {
+//     console.log("Score is\nComputer: " + computerScore + "\nYou: " + playerScore + "\nTry Again :(")
+// }
